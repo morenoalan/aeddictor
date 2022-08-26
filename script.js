7d54d411da893e74fc24a6a69e1764538263e1c4
Alan Moreno <alan.moreno@hotmail.com>
2022-08-25 23:17:59 -0300

    firsts sets for dark mode function

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function applyTextBold() {
     document.getElementById('textArea').value = document.getElementById('textArea').value.bold();
 }
+
+function changeMode(){
+    document.body.classList.toggle("body-dark-mode");
+    document.getElementById("main").classList.toggle("main-dark-mode");
+}
