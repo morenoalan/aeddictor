c7373b850bffca0f8d98410fa5120966dd9a75a7
Alan Moreno <alan.moreno@hotmail.com>
2022-08-30 01:59:16 -0300

    evolving buttons, dark mode and format align

--- script.js
+++ script.js
@@ -2,11 +2,32 @@ function applyTextBold() {
     document.getElementById('textArea').value = document.getElementById('textArea').value.bold();
 }
 
+var statusMode = "light";
 function changeMode(){
     document.body.classList.toggle("body-dark-mode");
     document.getElementById("main").classList.toggle("main-dark-mode");
+    if(statusMode == "light"){
+        document.getElementById("changeMode").children[0].innerText = "light_mode";
+        statusMode = "dark";
+    }else{
+        document.getElementById("changeMode").children[0].innerText = "dark_mode";
+        statusMode = "light";
+    }
 }
 
 function speechToText(){
 
 }
+
+function applyTextLeft(){
+    let formatAlignChildren = document.getElementById("formatAlign").children;
+    for(let i = 0; i < formatAlignChildren.length; i++){
+        document.getElementById("formatAlign").children[i].style.setProperty('transform', 'scale(1)');
+    }
+    //document.getElementById("formatAlign").getElementsByTagName("li")[1].style.setProperty('height', '-50px');
+}
+
+/*
+Change root
+document.documentElement.style.setProperty('--size-button', '40px');
+*/
